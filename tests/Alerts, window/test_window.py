60bf81bf586b06ca336db54dc64909d6bd5b4c2f
rgn2836 <bublik0706@gmail.com>
2025-09-06 18:41:20 +0300

    написал тесты на window_page

--- tests/Alerts, window/test_window.py
+++ tests/Alerts, window/test_window.py
@@ -6,9 +6,16 @@ url = URL()
 class TestWindowPage:
 
 
-    @pytest.mark.smoke
+    @pytest.mark.window_page
     def test_new_tab(self, window_page):
         window_page.open(url.Window_Page)
         window_page.click_to_tab_button()
         opened_new_tab = window_page.check_new_tab()
         assert opened_new_tab
+
+    @pytest.mark.window_page
+    def test_new_window(self, window_page):
+        window_page.open(url.Window_Page)
+        window_page.click_to_window_button()
+        opened_new_window = window_page.check_new_window()
+        assert opened_new_window
